09fbb10692a56f137f09beb251e5eefdffb7dad2
xyt <xuyuntao521@yeah.net>
2018-11-29 00:40:09 +0800

    默认的

--- 2/turtle_test.py
+++ 2/turtle_test.py
@@ -8,7 +8,23 @@ done（）保持程序不结束
 """
 
 import turtle
+turtle.speed(10)
 turtle.forward(100)
+turtle.right(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.goto(0,0)
+turtle.up()
+turtle.goto(-100,100)
+turtle.down()
+turtle.pensize(5)
+turtle.pencolor("red")
+turtle.goto(100,-100)
+while 1:
+
+    turtle.right(45)
+    turtle.circle(50)
 turtle.done()
 
 
